8af1552855e6b5f9b07a5ffcd7dd89c1052d35b1
longrui <ma0@duck.com>
2024-02-14 23:20:59 +0100

    correction: not phasor in mydsp, but phaser does input_sample*phaser_shift

--- rust-jack/mydsp-jack-conception/version0/phaser_version0.rs
+++ rust-jack/mydsp-jack-conception/version0/phaser_version0.rs
@@ -2,7 +2,7 @@ use crate::import::*;
 use crate::sine_table::SineTable;
 
 #[derive(Debug)]
-pub struct PhasorEffect {
+pub struct PhaserEffect {
     input_port: jack::Port<AudioIn>,
     output_port: jack::Port<AudioOut>,
     phase: f32,
@@ -10,9 +10,9 @@ pub struct PhasorEffect {
     sample_rate: f32,
 }
 
-impl PhasorEffect {
+impl PhaserEffect {
     pub fn new(input_port: jack::Port<AudioIn>, output_port: jack::Port<AudioOut>, frequency: f32, sample_rate: f32) -> Self {
-        PhasorEffect {
+        PhaserEffect {
             input_port,
             output_port,
             phase: 0.0,
@@ -22,22 +22,21 @@ impl PhasorEffect {
     }
 }
 
-impl ProcessHandler for PhasorEffect {
+impl ProcessHandler for PhaserEffect {
     fn process(&mut self, _: &Client, ps: &ProcessScope) -> Control {
         let input = self.input_port.as_slice(ps);
         let output = self.output_port.as_mut_slice(ps);
 
         for (i, out_sample) in output.iter_mut().enumerate() {
             let in_sample = input[i];
-            // let phasor_sample = (2.0 * std::f32::consts::PI * self.phase).sin();// without sineTable
+            // let phaser_sample = (2.0 * std::f32::consts::PI * self.phase).sin();// without sineTable
             let sinetable = SineTable::new(2048); //todo sinetable problem?
-            let phasor_sample = sinetable.get_value(self.phase);
-            
+            let phaser_sample = sinetable.get_value(self.phase);
             self.phase += self.frequency / self.sample_rate;
             if self.phase >= 1.0 {
                 self.phase -= 1.0;
             }
-            *out_sample = in_sample * phasor_sample;
+            *out_sample = in_sample * phaser_sample;
         }
         Control::Continue
     }
